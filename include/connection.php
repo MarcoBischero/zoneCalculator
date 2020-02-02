0851d1ed2d15e23083ec04f95f13e168531abec1
System Administrator <root@WorkAlexPro.fritz.box>
2020-02-02 01:15:54 +0100

    Maj

--- include/connection.php
+++ include/connection.php
@@ -11,7 +11,13 @@ $db_name = $DBname;
 #Do not edit
 //--------------------------------------------------------------------------------------------------
 #$conn = mysqli_connect($host, $user, $pwd) or die(mysqli_error($conn));
-$conn = mysqli_connect($host, $username, $password, $db) or die("Error message...");
+$conn = mysqli_connect($host, $user, $pwd, $db_name) or die("Error message...");
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 #mysqli_select_db ($db);
 #mysqli_select_db($db_name,$conn)  or die(mysqli_error($conn));
 define("CONN", $conn);
